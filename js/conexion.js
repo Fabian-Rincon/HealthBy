88220d3d2c0ca83e05af21dc14fb30b711017e79
Fabián Rincón <fabianrincon2006@gmail.com>
2024-08-03 16:16:01 -0500

    Agregando metodos para realizar consultas SQL a conexion.js

--- js/conexion.js
+++ js/conexion.js
@@ -1,7 +1,7 @@
-// Conexion con la Base de Datos
+// Conexion con la Base de Datos mediante libreria MySQL
 let mysql = require("mysql");
 
-// Datos de autentificasion
+// Autenticación con la Base de Datos
 let conexion = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -9,14 +9,37 @@ let conexion = mysql.createConnection({
     database: "healthby"
 });
 
-// Revisa posibles errores en la conexión
+// Revisor de conexión e indeificador de errores         
 conexion.connect(function(err){
     if(err){
         throw err;
+        conexion.end()
     } else {
         console.log("Conexión exitosa")
     }
-})
+});
 
-// Finaliza la conexion
+// Realizador de consultas e identificador de errores 
+const pacientes = "SELECT cor_ele_pac, cont_pac FROM `pacientes`";
+conexion.query(pacientes,function(error,registros){
+    if(error){
+        throw error;
+    }else{
+        console.log(registros);
+    }
+});
+
+// Realizador de consultas a indices especificos
+/*
+const pacientes = "SELECT * FROM `pacientes`";
+conexion.query(pacientes,function(error,registros){
+    if(error){
+        throw error;
+    }else{
+        console.log(registros[0].num_doc_pac);
+    }
+});
+*/
+
+// Finalizador de Conexión
 conexion.end()
