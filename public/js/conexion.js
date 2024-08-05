aff1c7eb9b9d4b39a716522d026417a142cb536f
Fabián Rincón <fabianrincon2006@gmail.com>
2024-08-05 09:54:39 -0500

    Agregando al archivo conexion.js método para modificar y consultar registros en la base de datos

--- public/js/conexion.js
+++ public/js/conexion.js
@@ -18,6 +18,7 @@ conexion.connect(function(err){
         console.log("Conexión exitosa")
     }
 });
+/* --------------------------------------------------------------------------- */
 
 /*
 // Realizador de consultas e identificador de errores 
@@ -41,8 +42,11 @@ conexion.query(PACIENTES,function(error,registros){
 });
 */
 
+/* --------------------------------------------------------------------------- */
+
+/* 
 // Agregar registro a base de datos y consultarlo mediante la cantidad total de registros -1
-const AGREGAR_REGISTRO = "INSERT INTO pacientes (num_doc_pac, nom_pac, ape_pac, fec_nac_pac, gen_pac, dir_pac, tel_pac, cor_ele_pac, fec_afi_pac, cont_pac, his_med_pac) VALUES ('1000000001', 'Pepito ', 'Perez', '2014-04-23', 'Masculino', 'Direccion de ejemplo Pepito', '3010000000', 'pepitoperez54@gmail.com', '2020-11-24', 'pepitoperez1111', NULL)";
+const AGREGAR_REGISTRO = "INSERT INTO pacientes (num_doc_pac, nom_pac, ape_pac, fec_nac_pac, gen_pac, dir_pac, tel_pac, cor_ele_pac, fec_afi_pac, cont_pac, his_med_pac) VALUES ('1000000003', 'Pepito ', 'Perez', '2014-04-23', 'Masculino', 'Direccion de ejemplo Pepito', '3010000000', 'pepitoperez54@gmail.com', '2020-11-24', 'pepitoperez1111', NULL)";
 conexion.query(AGREGAR_REGISTRO, function(error,nuevoRegistro){
     if(error){
         throw error;
@@ -51,6 +55,7 @@ conexion.query(AGREGAR_REGISTRO, function(error,nuevoRegistro){
     }
 });
 
+// Mostrar ultimo registro agregado
 const PACIENTES = "SELECT * FROM pacientes";
 conexion.query(PACIENTES,function(error,registros){
     if(error){
@@ -59,6 +64,31 @@ conexion.query(PACIENTES,function(error,registros){
         console.log(registros[registros.length-1]);
     }
 });
+ */
+
+/* --------------------------------------------------------------------------- */
+
+// Modificar un registro ya agregado a la base de datos y consultarlo
+const MODIFICAR = "UPDATE pacientes SET nom_pac = 'juanito', ape_pac ='Molina', dir_pac = 'Dirreción de Juanito', cor_ele_pac = 'juanitomolina@gmail.com', cont_pac = 'juanitomolina1234' WHERE pacientes.num_doc_pac = '1000000003'"
+conexion.query(MODIFICAR, function(error,registros){
+    if(error){
+        throw error;
+    }else{
+        console.log("Datos modificados correctamente");
+    }
+});
+
+// Consultar todos los registros 
+const PACIENTES = "SELECT * FROM pacientes";
+conexion.query(PACIENTES,function(error,registros){
+    if(error){
+        throw error;
+    }else{
+        console.log(registros);
+    }
+});
+
+/* --------------------------------------------------------------------------- */
 
 // Finalizador de Conexión
 conexion.end()
