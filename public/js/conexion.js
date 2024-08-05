72367bf1d7818edc4b57319624302bb657a07d35
Fabián Rincón <fabianrincon2006@gmail.com>
2024-08-05 10:18:13 -0500

    Agregando al archivo conexion.js método eliminar registros en la base de datos

--- public/js/conexion.js
+++ public/js/conexion.js
@@ -46,7 +46,7 @@ conexion.query(PACIENTES,function(error,registros){
 
 /* 
 // Agregar registro a base de datos y consultarlo mediante la cantidad total de registros -1
-const AGREGAR_REGISTRO = "INSERT INTO pacientes (num_doc_pac, nom_pac, ape_pac, fec_nac_pac, gen_pac, dir_pac, tel_pac, cor_ele_pac, fec_afi_pac, cont_pac, his_med_pac) VALUES ('1000000003', 'Pepito ', 'Perez', '2014-04-23', 'Masculino', 'Direccion de ejemplo Pepito', '3010000000', 'pepitoperez54@gmail.com', '2020-11-24', 'pepitoperez1111', NULL)";
+const AGREGAR_REGISTRO = "INSERT INTO pacientes (num_doc_pac, nom_pac, ape_pac, fec_nac_pac, gen_pac, dir_pac, tel_pac, cor_ele_pac, fec_afi_pac, cont_pac, his_med_pac) VALUES ('1000000007', 'Pepito ', 'Perez', '2014-04-23', 'Masculino', 'Direccion de ejemplo Pepito', '3010000000', 'pepitoperez54@gmail.com', '2020-11-24', 'pepitoperez1111', NULL)";
 conexion.query(AGREGAR_REGISTRO, function(error,nuevoRegistro){
     if(error){
         throw error;
@@ -65,9 +65,9 @@ conexion.query(PACIENTES,function(error,registros){
     }
 });
  */
-
 /* --------------------------------------------------------------------------- */
 
+/* 
 // Modificar un registro ya agregado a la base de datos y consultarlo
 const MODIFICAR = "UPDATE pacientes SET nom_pac = 'juanito', ape_pac ='Molina', dir_pac = 'Dirreción de Juanito', cor_ele_pac = 'juanitomolina@gmail.com', cont_pac = 'juanitomolina1234' WHERE pacientes.num_doc_pac = '1000000003'"
 conexion.query(MODIFICAR, function(error,registros){
@@ -79,6 +79,29 @@ conexion.query(MODIFICAR, function(error,registros){
 });
 
 // Consultar todos los registros 
+const PACIENTES = "SELECT * FROM pacientes";
+conexion.query(PACIENTES,function(error,registros){
+    if(error){
+        throw error;
+    }else{
+        console.log(registros);
+    }
+});
+ */
+
+/* --------------------------------------------------------------------------- */
+
+// Eliminar registros en la base de datos
+const ELIMINAR = "DELETE FROM pacientes WHERE pacientes.num_doc_pac = '1000000007'"
+conexion.query(ELIMINAR, function(error,registros){
+    if(error){
+        throw error;
+    }else{
+        console.log("Datos eliminados correctamente")
+    }
+})
+
+//Consultar todos los registros
 const PACIENTES = "SELECT * FROM pacientes";
 conexion.query(PACIENTES,function(error,registros){
     if(error){
