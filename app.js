b8611edcfa303af5f6b15cfa8b8d94f9d332a034
michaelfa7 <maicoleduardofigueroa@gmail.com>
2024-08-06 14:53:59 -0500

    mejoras para ordenar el codigo

--- app.js
+++ app.js
@@ -1,52 +1,25 @@
 // importamos libreria
-const express = require("express");
+const express = require('express');
 
 // llamar los metodos de express
 const app = express();
 
 // motor de visualizacion ejs
-app.set("view engine","ejs");
+app.set('view engine','ejs');
 
-// res.render para cargar archivos ejs 
-// index page
-app.get("/", function(req,res){
-    res.render("index");
-});
-// home page
-app.get('/pages/home', function(req, res){
-    res.render('pages/home');
-});
-// login page
-app.get('/pages/login', function(req, res){
-    res.render('pages/login');
-});
-// recovery page
-app.get('/pages/recovery', function(req, res){
-    res.render('pages/recovery');
-});
-// register page
-app.get('/pages/register', function(req, res){
-    res.render('pages/register');
-});
+// llamada al archivo router
+app.use('/', require('./router'));
 
-/* 
-rutas ejemplos
+// ejemplos de ruta cuando solo se usa el archivo app.js
+// mostra en pantalla hola mundo, ruta de prueba
+// app.get('/', (req,res)=>{
+//     res.send('<h1> Hola Mundo');
+// });
 
-mostra en pantalla, ruta de prueba
-app.get("/",function(req,res){
-    res.send("<h1> Hola Mundo");
-});
-
-main page
-app.get('/pages/main', function(req, res){
-    res.render('pages/main');
-});
-*/
-
-// middleware
-app.use(express.static("public"));
+// middleware, para conectar los estilos en la carpeta public
+app.use(express.static('public'));
 
 // configurar puerto para el servidor local
-app.listen(3000,function(){
-    console.log("servidor http://localhost:3000");
+app.listen(3000, ()=>{
+    console.log('servidor http://localhost:3000');
 });
